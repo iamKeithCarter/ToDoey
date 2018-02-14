94658af4e6a13309dfe38e974949d2210fb3d38c
Keith Carter <jake@Keith-Carters-iMac.local>
2018-02-14 11:00:46 -0500

    decoder complete

--- ToDoey/Cotrollers/TodoeyTableViewController.swift
+++ ToDoey/Cotrollers/TodoeyTableViewController.swift
@@ -14,7 +14,7 @@ class TodoeyTableViewController: UITableViewController {
     
     let defaultskc = UserDefaults.standard
     
-    let dateFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,17 +24,19 @@ class TodoeyTableViewController: UITableViewController {
         
        // print(dateFilePath!)
         
-        let newItem = Itm()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
+//        let newItem = Itm()
+//        newItem.title = "Find Mike"
+//        itemArray.append(newItem)
+//        
+//        let newItem2 = Itm()
+//        newItem2.title = "stop Mike"
+//        itemArray.append(newItem2)
+//        
+//        let newItem3 = Itm()
+//        newItem3.title = "capture Mike"
+//        itemArray.append(newItem3)
         
-        let newItem2 = Itm()
-        newItem2.title = "stop Mike"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Itm()
-        newItem3.title = "capture Mike"
-        itemArray.append(newItem3)
+        loadItems()
         
 //        if let itemskc = defaultskc.array(forKey: "TodoListArray") as? [Itm]{
 //            itemArray = itemskc
@@ -131,11 +133,23 @@ class TodoeyTableViewController: UITableViewController {
         do{
             let data = try encoder.encode(
                 itemArray)
-            try data.write(to: dateFilePath!)
+            try data.write(to: dataFilePath!)
         }catch{
             print("Error encoding item array,\(error)")
         }
         self.tableView.reloadData()
     }
     
+    func loadItems() {
+        if let data = try? Data (contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            do{
+            itemArray = try decoder.decode([Itm].self, from: data)
+        } catch {
+            print("errors")
+       
+        }
+    }
+    
+}
 }
